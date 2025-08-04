31774d3e20e9a270ff9e87df8b5d6e584c4c0cab
Qasem Ehab kamel Aboalia <qassimalia77@gmail.com>
2025-08-04 15:09:11 +0300

    Add files via upload

---
+++ 0026.Remove Duplicates from Sorted Array/script.js
@@ -0,0 +1,9 @@
+var removeDuplicates = function(nums){
+    let index=1;
+    for (let i=1;i<nums.length;i++){
+        if (nums[i] !==nums[i-1]){
+            nums[index++]=nums[i];
+        }
+    }
+    return index
+};
