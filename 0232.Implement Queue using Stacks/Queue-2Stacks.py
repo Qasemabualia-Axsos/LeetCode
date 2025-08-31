9c46ca9975c474f208595146feaa6c2420ced593
Qasem Ehab kamel Aboalia <qassimalia77@gmail.com>
2025-08-31 14:05:29 +0300

    Add files via upload

---
+++ 0232.Implement Queue using Stacks/Queue-2Stacks.py
@@ -0,0 +1,28 @@
+class MyQueue(object):
+    def __init__(self):
+        self.stack1=[]
+        self.stack2=[]
+
+    def push(self,x):
+        self.stack1.append(x)
+
+    
+    def pop (self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop(()))
+
+        return self.stack2.pop()
+    
+    def peek(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+                
+        return self.stack2[-1]
+    
+
+    def empty(self):
+        if len(self.stack1)==0 and len(self.stack2)==0:
+            return True
+        return False
